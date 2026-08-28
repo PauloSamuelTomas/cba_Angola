4e15048ed3fd1e194bf09ad5bd7fc57d71f91e3d
Paulo Tomas <cientistapaulopaulo@gmail.com>
2026-08-28 11:57:01 +0100

    configurado o cors para o frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,8 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:5500",
-            "http://localhost:5500"
+    origins "*"
 
     resource "*",
       headers: :any,
